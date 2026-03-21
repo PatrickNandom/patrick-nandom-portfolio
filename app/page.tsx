8fdd2fbf2999e9f400695f88c4b9f3b40c732e44
Nandom Patrick Molshakat <139545102+PatrickNandom@users.noreply.github.com>
2026-03-21 19:24:33 +0100

    add tailwind CSS to skills section

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,7 @@ export default function Home() {
         "Javascript",
         "React",
         "NextJs",
+        "Tailwind CSS",
         "TypeScript",
         "NodeJs",
         "Spring Boot",
